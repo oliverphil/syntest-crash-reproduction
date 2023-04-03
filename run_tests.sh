b334459f4cd765ae7af2adc52d7cc9e7649603ff
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2023-04-04 11:59:04 +1200

    Update run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,6 @@ project_array=[ 'atom', 'eslint', 'express', 'http-server', 'node', 'standard', 
 for (( i = 0; i < 15; i++ )); do
   for (( proj_index = 0; proj_index < ${project_array[@]}; proj_index++ )); do
     export SYNTEST_PROJECT=project_array[proj_index];
-    npm run run > output_$(SYNTEST_PROJECT)_$((i + 1)).log 2> stderr_$(SYNTEST_PROJECT)_$((i + 1)).log
+    npm run run > output_$((SYNTEST_PROJECT))_$((i + 1)).log 2> stderr_$((SYNTEST_PROJECT))_$((i + 1)).log
   done
 done
