fe7eafe391c8f320c0d08b285b7f6889dfeb88f3
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2023-04-04 11:49:25 +1200

    Fix Project Loop in Grid Script

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ npm run build
 project_array=[ 'atom', 'eslint', 'express', 'http-server', 'node', 'standard', 'webpack' ];
 
 for (( i = 0; i < 15; i++ )); do
-  for (( proj_index = 0; proj_index < 5; proj_index++ )); do
+  for (( proj_index = 0; proj_index < ${project_array[@]}; proj_index++ )); do
     export SYNTEST_PROJECT=project_array[proj_index];
     npm run run > output_$(SYNTEST_PROJECT)_$((i + 1)).log 2> stderr_$(SYNTEST_PROJECT)_$((i + 1)).log
   done
