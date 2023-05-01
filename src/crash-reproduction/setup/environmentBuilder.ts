cce58a552fed19cf7234516b8c738c251005f8ac
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2023-05-02 11:16:17 +1200

    Remove Node Versioning For Crash Installation

--- src/crash-reproduction/setup/environmentBuilder.ts
+++ src/crash-reproduction/setup/environmentBuilder.ts
@@ -41,11 +41,11 @@ class EnvironmentBuilder {
     let stderr = '';
     let error = false;
     try {
-      if (crash.nodeVersion) {
-        stdout = execSync(`unset npm_config_prefix && source /vol/grid-solar/sgeusers/oliverphil/.nvm/nvm.sh && nvm install ${crash.nodeVersion} && npm --prefix ${crashFolder} i`).toString();
-      } else {
+      // if (crash.nodeVersion) {
+      //   stdout = execSync(`unset npm_config_prefix && source /vol/grid-solar/sgeusers/oliverphil/.nvm/nvm.sh && nvm install ${crash.nodeVersion} && npm --prefix ${crashFolder} i`).toString();
+      // } else {
         stdout = execSync(`npm --prefix ${crashFolder} i`).toString();
-      }
+      // }
     } catch (e) {
       stdout = e.stdout.toString();
       stderr = e.stderr.toString();
