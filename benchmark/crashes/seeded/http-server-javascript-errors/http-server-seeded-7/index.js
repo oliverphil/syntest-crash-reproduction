1f90f5519519ebc0b77a4aa0ca4f5aabf335a0bc
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2023-05-25 13:51:21 +1200

    Add Benchmark

---
+++ benchmark/crashes/seeded/http-server-javascript-errors/http-server-seeded-7/index.js
@@ -0,0 +1,23 @@
+const run = async () => {const imp = await import('http-server/lib/http-server.js');
+console.log('http-server/lib/http-server.js:createServer');
+var func = imp.default.createServer || imp.default.prototype.createServer;
+const ctor = imp.default.constructor || imp.default.prototype.constructor;
+const obj = createObject(ctor);
+func = func.bind(obj);
+const res = func(2);
+console.log(res);
+};
+run().then(() => process.exit(0));
+function createObject(ctor) {
+  var obj, newObj;
+
+  function fakeCtor() {}
+  fakeCtor.prototype = ctor.prototype;
+  obj = new fakeCtor();
+  obj.constructor = ctor;
+  newObj = ctor.apply(obj);
+  if (newObj && (typeof newObj === 'object' || typeof newObj === 'function')) {
+    return newObj;
+  }
+  return obj;
+}
