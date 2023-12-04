ca6a9d082adeff4f2c26f44d4be7d275dd522bae
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2023-12-04 15:55:04 +1300

    Reduce Targets

--- tools/crash-reproduction/lib/FileSelector.ts
+++ tools/crash-reproduction/lib/FileSelector.ts
@@ -4,7 +4,7 @@ import {JavaScriptArguments} from "./CrashLauncher";
 
 export class FileSelector extends CoreFileSelector {
     loadCrashFilePaths(exclude: string[], crash: Crash, arguments_: JavaScriptArguments): Set<string> {
-        const include: string[] = [`./benchmark/crashes/${arguments_.syntestType}/${crash.project}/${crash.crashId}/**/*.js`];
+        const include: string[] = [] //[`./benchmark/crashes/${arguments_.syntestType}/${crash.project}/${crash.crashId}/**/*.js`];
         for (const frame of crash.stackTrace.trace) {
             if (frame.file.includes('.js')) {
                 const crashFile = `./benchmark/crashes/${arguments_.syntestType}/${crash.project}/${crash.crashId}/**/${frame.file}`;
