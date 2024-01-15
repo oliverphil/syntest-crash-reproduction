6d25aa848a3f321007226e7be8b14b51de2c7a22
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2024-01-16 11:33:08 +1300

    Update Queue Script

--- queue_experiment.sh
+++ queue_experiment.sh
@@ -2,11 +2,12 @@ echo "$1"
 echo "$2"
 echo "$3"
 
-if [ "$3" -ge 15 ]; then
+if [ "$3" -ge 30 ]; then
+  echo "Timeout on \"$1\""
   qdel -u oliverphil
 fi
 
-if qstat | grep -P 'oliverphil\s*[qwrT]+\s*' ; then
+if qstat | grep -P 'oliverphil\s*[qwr]+\s*(?:(?:[\d\/\s:\.]+all.q@two-rooms)|(?:\d)+)' ; then
   echo "Jobs Running"
   echo ./queue_experiment.sh "$1" "$2" $(("$3" + 1)) | at now + 30 minutes
   atq
@@ -17,18 +18,21 @@ echo "Delete"
 qdel -u oliverphil
 
 if [ "$1" -gt 1 ]; then
+  echo "Save Results From $1"
   tar czf /vol/grid-solar/sgeusers/oliverphil/results/output_"$(($1 - 1))".tar.gz /vol/grid-solar/sgeusers/oliverphil/output
   rm /vol/grid-solar/sgeusers/oliverphil/output/*
   ls /vol/grid-solar/sgeusers/oliverphil/results
 fi
 
 if [ "$1" -ge "$2" ]; then
-  echo "Complete"
+  echo "Full Run Complete"
   exit 0
 fi
 
-echo "Test Completed"
+echo "Starting Run For $1"
 node run_per_crash.mjs "$1"
 echo ./queue_experiment.sh $(("$1" + 1)) "$2" 1 | at now + 30 minutes
 qstat
 atq
+echo "Success"
+exit 0
